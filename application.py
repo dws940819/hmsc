6beb22227c91545127b319c79ade31925c2c0506
dws <695036039@qq.com>
2020-04-22 12:26:21 +0800

    '静态文件加载完成'

--- application.py
+++ application.py
@@ -4,13 +4,15 @@ import os
 
 
 class Applciation(Flask):
-    def __init__(self,import_name, template_folder=None, root_path=None):
-        super(Applciation, self).__init__(import_name, template_folder=template_folder, root_path=root_path)
+    def __init__(self, import_name, template_folder=None, root_path=None):
+        super(Applciation, self).__init__(import_name, template_folder=template_folder, root_path=root_path,static_folder=None)
 
 
 app = Applciation(__name__, template_folder=os.getcwd()+'/web/templates/', root_path=os.getcwd())
 manager = Manager(app)
 
 
-
+from common.libs.UrlManager import UrlManager
+app.add_template_global(UrlManager.buildStaticUrl,'buildStaticUrl')
+app.add_template_global(UrlManager.buildUrl,'buildUrl')
 
---
+++ web/static/js/common.js
@@ -0,0 +1,16 @@
+;
+var common_ops = {
+    init:function(){
+        console.log('common.js初始化')
+    },
+
+}
+
+
+$(document).ready(function(){
+    common_ops.init();
+})
+
+
+
+
