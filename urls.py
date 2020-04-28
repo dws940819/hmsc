939e2f69a4b41850c38be8bbc2ec6527f10b6d78
dws <695036039@qq.com>
2020-04-28 10:21:29 +0800

    '更新商品管理模板'

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from application import app
 from web.controllers.static import route_static
 from web.controllers.index import route_index
 from web.controllers.account.Account import router_account
+from web.controllers.goods.Goods import router_goods
 
 
 # 拦截器路由
@@ -10,6 +11,7 @@ from web.interceptors.AuthInterceptor import *
 
 app.register_blueprint(router_user,url_prefix='/user')
 app.register_blueprint(route_static,url_prefix="/static")
+app.register_blueprint(router_goods,url_prefix="/goods")
 app.register_blueprint(route_index,url_prefix="/")
 app.register_blueprint(router_account,url_prefix="/account")  
 
